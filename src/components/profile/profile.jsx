3ef02e5b1e136e1297ac146aa2a9c9af105e56bf
npc_unit <o_xx@list.ru>
2023-11-15 01:31:30 +0300

    create profile

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,9 +1,19 @@
 import { Link, useLocation } from "react-router-dom";
 import styles from "./profile.module.scss";
 import cn from "classnames";
+import { EmailInput, PasswordInput } from "@ya.praktikum/react-developer-burger-ui-components";
+import { useState } from "react";
 
 export const Profile = () => {
   const pathname = useLocation().pathname;
+
+  const [name, setName] = useState("");
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChange = (e, setState) => {
+    setState(e.target.value);
+  };
 
   return (
     <article className={styles.profile}>
@@ -34,8 +44,13 @@ export const Profile = () => {
             </Link>
           </li>
         </ul>
+        <p className={"text text_type_main-default text_color_inactive"}>В этом разделе вы можете изменить свои персональные данные</p>
       </nav>
-      <form className={styles.profile__form}></form>
+      <form className={styles.profile__form}>
+        <EmailInput onChange={(e) => handleChange(e, setName)} value={name} name={"name"} placeholder="Имя" isIcon={true} error={false} extraClass="mb-2" />
+        <EmailInput onChange={(e) => handleChange(e, setLogin)} value={login} name={"login"} placeholder="Логин" isIcon={true} extraClass="mb-2" />
+        <PasswordInput onChange={(e) => handleChange(e, setPassword)} value={password} name={"password"} icon="EditIcon" />
+      </form>
     </article>
   );
 };
