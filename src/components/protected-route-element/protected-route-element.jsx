87a59b73d6aedc020e8ea548edc4488203549d2e
npc_unit <o_xx@list.ru>
2023-11-22 19:40:46 +0300

    added propTypes for protected-route-element

--- src/components/protected-route-element/protected-route-element.jsx
+++ src/components/protected-route-element/protected-route-element.jsx
@@ -1,5 +1,12 @@
 import { Navigate } from "react-router-dom";
+import PropTypes from "prop-types";
 
 export const ProtectedRouteElement = ({ element, property, path }) => {
   return property ? element : <Navigate to={path} replace />;
+};
+
+ProtectedRouteElement.propTypes = {
+  element: PropTypes.element.isRequired,
+  property: PropTypes.string.isRequired,
+  path: PropTypes.string.isRequired,
 };
